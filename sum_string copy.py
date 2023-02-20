428ecf9046dfb5b42a88a2f3ac29d66d4f4b80fc
donsbx0 <doanhhoang710@gmail.com>
2023-02-20 09:27:58 -0800

    add file

---
+++ sum_string copy.py
@@ -0,0 +1,13 @@
+print('Nhao vao cac so co khoang trang: ')
+dayGiatri = input()
+danhsachGiatri = dayGiatri.split()
+Ipokla = False
+try:
+    danhsachSo = map(int, danhsachGiatri)
+    tongDayso = sum(danhsachSo)
+    Ipokla = True
+except:
+    print('Gia tri nhap vao khong hiop le')
+
+if Ipokla:
+    print('Tong dang so la: ', tongDayso)
